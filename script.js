517f198303bb8bce6bbce6b501c511227d5ef881
Amartya Sinha <amartua2002@gmail.com>
2023-09-18 17:52:27 +0530

    feat: camera

--- script.js
+++ script.js
@@ -86,3 +86,29 @@ document.addEventListener("DOMContentLoaded", () => {
         questionsSection.classList.toggle("hidden");
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    let but = document.getElementById("but");
+    let video = document.getElementById("vid");
+    let mediaDevices = navigator.mediaDevices;
+    vid.muted = true;
+    but.addEventListener("click", () => {
+
+        // Accessing the user camera and video.
+        mediaDevices
+            .getUserMedia({
+                video: true,
+                audio: true,
+            })
+            .then((stream) => {
+
+                // Changing the source of video to current stream.
+                video.srcObject = stream;
+                video.addEventListener("loadedmetadata", () => {
+                    video.play();
+                });
+            })
+            .catch(alert);
+    });
+});
